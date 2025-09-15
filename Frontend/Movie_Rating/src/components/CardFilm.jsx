825761747b09581536a6e65ddf4ac01be78dcb36
OthmaneBk <okebennani@gmail.com>
2025-09-15 06:56:52 +0200

    change the compoenet

--- Frontend/Movie_Rating/src/components/CardFilm.jsx
+++ Frontend/Movie_Rating/src/components/CardFilm.jsx
@@ -1,22 +1,22 @@
 import Card from 'react-bootstrap/Card';
 
 
-function CardFilm({ ...film}) {
-
-    return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>{film.nom}</Card.Title>
-          <Card.Text>
-            <span><strong>Réalisateur</strong>: {film.realisateur}</span><br/>
-            <span><strong>synopsis</strong>: {film.synopsis}</span><br/>
-            <span><strong>date_realisation</strong>: {film.date_realisation}</span><br/>
-            <span><strong>date_sortie</strong>: {film.date_sortie}</span><br/>
-            <span><strong>acteurs</strong>: {film.acteurs.join(', ')}</span><br/>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    );
+function CardFilm({ ...film }) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{film.name}</Card.Title>
+        <Card.Text>
+          <span><strong>Réalisateur</strong>: {film.realisateur}</span><br />
+          <span><strong>Synopsis</strong>: {film.synopsis}</span><br />
+          <span><strong>Date de réalisation</strong>: {film.dateDeRealisation}</span><br />
+          <span><strong>Date de sortie</strong>: {film.dateDeSortie}</span><br />
+          <span><strong>Acteurs</strong>: {film.listeDesActeurs.join(', ')}</span><br />
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
+
 
 export default CardFilm
