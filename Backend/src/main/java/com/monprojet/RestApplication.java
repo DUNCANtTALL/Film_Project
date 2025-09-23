a519d0f883419da18f1c3beb7364b3ab73bb13b4
Nada-Hl <nada.ellehlaissi@gmail.com>
2025-09-23 23:05:35 +0200

    Backend new version

---
+++ Backend/src/main/java/com/monprojet/RestApplication.java
@@ -0,0 +1,9 @@
+package com.monprojet;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api") // Base path for all APIs
+public class RestApplication extends Application {
+    // No code needed, just configuration
+}
